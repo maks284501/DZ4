eafc5da16191d00a6eabaddb9090452dd190e7a2
Maks <orbita4501@gmail.com>
2023-01-16 22:14:34 +0300

    done

---
+++ задание1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Запрос числа от пользователя
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num=0;
+
+// Условие
+while (number>0)
+{
+  num=((number%10)+num);
+ number=number/10;
+  
+
+
+}
+
+Console.WriteLine($"Количество: {num}");
---
+++ задание3/Program.cs
@@ -0,0 +1,18 @@
+﻿//задаём переменные
+ int[] masiv = new int[8]{0,0,0,0,0,0,0,0}; // объявляем массив
+       int n=0;
+
+//запрашиваем цифры 8 раз подряд
+while(n<8)
+
+{Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ masiv[n] = number;
+ n=n+1;
+}     
+            
+            
+ //Вывод массива         
+ Console.Write("Array: ");
+ for(int i = 0; i<masiv.Length; i++)
+Console.Write(" " + masiv[i]); 
